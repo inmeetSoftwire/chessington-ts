ed65b86cec67a716f23b104e61448e10cdd12f46
Inmeet Khurana <inmeet.khurana@softwire.com>
2024-06-26 17:01:56 +0100

    Bishops can move diagonally and cannot make any other moves.

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,27 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moveArray = []
+        const currentRow = board.findPiece(this).row
+        const currentCol = board.findPiece(this).col
+        for (let i = 1; i < 8; i++) {
+            if (currentRow + i < 8) {
+                if (currentCol + i < 8) {
+                    moveArray.push(Square.at(currentRow + i, currentCol + i))
+                }
+                if (currentCol - i >= 0) {
+                    moveArray.push(Square.at(currentRow + i, currentCol - i))
+                }
+            }
+            if (currentRow - i >= 0) {
+                if (currentCol + i < 8) {
+                    moveArray.push(Square.at(currentRow - i, currentCol + i))
+                }
+                if (currentCol - i >= 0) {
+                    moveArray.push(Square.at(currentRow - i, currentCol - i))
+                }
+            }
+        }
+        return moveArray
     }
 }
