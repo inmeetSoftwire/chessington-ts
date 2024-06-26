39a82e7dc0e04b8e6e913b73d8b207539f0b2207
Inmeet Khurana <inmeet.khurana@softwire.com>
2024-06-26 16:52:35 +0100

    Rooks can move laterally and cannot make any other moves.

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const moveArray = []
+        for (let i = 0; i < 8; i++) {
+            if (i != currentSquare.col) {
+                moveArray.push(Square.at(currentSquare.row, i))
+            }
+            if (i != currentSquare.row) {
+                moveArray.push(Square.at(i, currentSquare.col))
+            }
+        } 
+        return moveArray
     }
 }
